ceca1401d728c8ebad4c7bf743e68bd570df2338
Nathaniel Dodson <ndodson79605@gmail.com>
2022-03-19 19:12:18 -0400

    organized directory, added more to classes and instructors

--- app.py
+++ app.py
@@ -3,21 +3,18 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 # Defining the home page of our site
-@app.route("/index.html")  # this sets the route to this page
-def home():
-	return render_template("/index")
+@app.route("/index")  
+def index():
+	return render_template("index.html")
 
-@app.route("/instructors.html")
-def instructors():
-    return render_template("instructors.html")
 
-@app.route("/classes.html")
+@app.route("/classes")
 def classes():
     return render_template("classes.html")
 
-@app.route("/pricing.html")
-def pricing():
-    return render_template("pricing.html")
+@app.route("/instructors")
+def instructors():
+    return render_template("instructors.html")
 
 if __name__ == "__main__":
     app.run()
